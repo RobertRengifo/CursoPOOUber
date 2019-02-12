90297fa85849e053710e87ae48ede69a633442a4
Andres Rengifo <robert.suarez.rengifo@gmail.com>
2019-02-12 03:54:39 +0000

    Nueva sintaxis para declarar clase y objetos

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,7 @@
 var car = new Car("AMQ123", new Account("Andres Herrera", "Andres123"))
-debugger;
+//debugger;
 car.passenger = 4;
 car.printDataCar();
+
+var car2 = new Car("SQR456", new Account("Jose Chavez", "Suarez123"))
+car2.printDataCar()
