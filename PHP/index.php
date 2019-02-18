eb86b25e4c9c261f6d93190ada13896da4e76e43
Andres Rengifo <robert.suarez.rengifo@gmail.com>
2019-02-18 00:42:06 +0000

    Agregando herencia a phyton

--- PHP/index.php
+++ PHP/index.php
@@ -7,6 +7,6 @@ require_once("account.php");
 $uberX = new UberX("ZPQ104", new Account("Andres Suarez", "AND456"),"Chevrolet", "Spark");
 $uberX->printDataCar();
 
-$uberPool = new UberX("ZPQ566", new Account("Suarez", "ANF345"),"Renult", "Uva");
+$uberPool = new UberPool("ZPQ566", new Account("Suarez", "ANF345"),"Renult", "Uva");
 $uberPool->printDataCar();
 ?>
